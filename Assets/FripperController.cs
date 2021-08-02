2423e10e7c1c9dcead3a6c277177a7d0dc25ff7c
curln <87195095+curln@users.noreply.github.com>
2021-08-02 21:39:06 +0900

    あきらめた人

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -11,7 +11,6 @@ public class FripperController : MonoBehaviour
     private float defaultAngle = 20;
     //弾いた時の傾き
     private float flickAngle = -20;
-    Vector2 vector2;
 
     // Use this for initialization
     void Start()
@@ -26,26 +25,6 @@ public class FripperController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        vector2 = touch.position;
-        if (TouchPhase.Began && vector2.x > 0&&tag=="LeftFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        else if (vector2.x < 0 && tag == "RightFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        else if (TouchPhase&&vector2.x < 0 && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        else if (TouchPhase.Began && vector2.x < 0 && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-
-
 
         //左矢印キーを押した時左フリッパーを動かす
         if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
